0f9bdc7ebe9c456cdee68e28dba0aa53f577d046
Julia Kurianova <15789859+judy99@users.noreply.github.com>
2026-07-11 20:06:25 -0400

    fix diary.py

--- assignment2/diary.py
+++ assignment2/diary.py
@@ -6,15 +6,14 @@ init_question = "What happened today? "
 question = "What else? "
 stop_line = "done for now"
 current_text = ""
-result_text = ""
 
 try:
     with open('diary.txt', 'a') as file:
         current_text = input(init_question)
         while current_text != stop_line:
-            result_text = result_text + current_text  + "\n"
+            file.write(current_text + '\n')
             current_text = input(question)
-        file.write(result_text)
+        
 except Exception as e:
     trace_back = traceback.extract_tb(e.__traceback__)
     stack_trace = list()
@@ -25,5 +24,3 @@ except Exception as e:
     if message:
         print(f"Exception message: {message}")
     print(f"Stack trace: {stack_trace}")
-
-# Task 2: Read a CSV File
